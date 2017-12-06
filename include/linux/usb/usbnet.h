c13660009f99a77e5d8437db37e9c2a37ed5746c
Bjørn Mork <bjorn@mork.no>
2017-12-06 20:21:24 +0100

    usbnet: fix alignment for frames with no ethernet header
    The qmi_wwan minidriver support a 'raw-ip' mode where frames are
    received without any ethernet header. This causes alignment issues
    because the skbs allocated by usbnet are "IP aligned".
    
    Fix by allowing minidrivers to disable the additional alignment
    offset. This is implemented using a per-device flag, since the same
    minidriver also supports 'ethernet' mode.
    
    Fixes: 32f7adf633b9 ("net: qmi_wwan: support "raw IP" mode")
    Reported-and-tested-by: Jay Foster <jay@systech.com>
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Original commit in Linux upstream is
    a4abd7a80addb4a9547f7dfc7812566b60ec505c.

--- include/linux/usb/usbnet.h
+++ include/linux/usb/usbnet.h
@@ -75,6 +75,7 @@ struct usbnet {
 #		define EVENT_NO_RUNTIME_PM	9
 #		define EVENT_RX_KILL	10
 #		define EVENT_LINK_CHANGE	11
+#		define EVENT_NO_IP_ALIGN	13
 };
 
 static inline struct usb_driver *driver_of(struct usb_interface *intf)
